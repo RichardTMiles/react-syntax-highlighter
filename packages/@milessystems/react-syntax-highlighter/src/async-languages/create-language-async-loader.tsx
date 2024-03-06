ac7995ac19bd68c21f8aee9e2d009054d5479183
richardtmiles <richard@miles.systems>
2024-03-06 02:33:34 -0700

    publish 15.5.7

---
+++ packages/@milessystems/react-syntax-highlighter/src/async-languages/create-language-async-loader.tsx
@@ -0,0 +1,8 @@
+export default <T,>(name: string, loader: () => Promise<T>) => {
+  return async (registerLanguage : (name:string, module: any) => void)  => {
+    const module = await loader();
+    // Use a type assertion here to indicate that module might have a default property
+    const mod = module as T & { default?: T };
+    registerLanguage(name, mod.default || module);
+  };
+};
